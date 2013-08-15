73d7aa68250264b425bda110cf68d300f05af29f
chagan <chrishagan23@gmail.com>
2013-08-15 16:56:59 -0700

    cleaning up console.logs

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ io.configure(function () {
 });
 
 app.get('/hook/instagram', function(req, res){
-    console.log("got a request");
-    console.log(req.query);
 	if (req.query['hub.mode'] == 'subscribe'){
 		res.send(req.query['hub.challenge']);
 	} else {
