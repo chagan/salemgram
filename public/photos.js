851dea6bc4eaef4ee7f7bb73c74ba0579bbba5f3
chagan <chrishagan23@gmail.com>
2013-08-21 14:55:00 -0700

    added user sorting

--- public/photos.js
+++ public/photos.js
@@ -2,11 +2,16 @@ function Ctrl($scope) {
 
 	$scope.photos = [];
 	
+	$scope.users = [];
+	
 	$scope.socket = io.connect(window.location.protocol + "//" + window.location.host);
 		
 	$scope.socket.on('photos', function(photos) {
 		$scope.$apply(function(scope) {
 			scope.photos = photos;
+			angular.forEach(photos, function(photo){
+				GetUsers(photo);
+			})
 		});
 	});
 	
@@ -14,7 +19,24 @@ function Ctrl($scope) {
 		console.log('calling scope new')
 		$scope.$apply(function(scope){			
 			scope.photos.push(photo);
+			GetUsers(photo);
 		});
 	});
-
+	
+	$scope.userSearch = function(userselect) {
+	  return function(photo) {
+	  	if (!userselect) {
+	  		return true;
+	  	} else {
+			return photo.user.username === userselect;
+		}
+	  };
+	};
+	
+	var GetUsers = function(photo){
+		if ($scope.users.indexOf(photo.user.username) == -1) {
+			$scope.users.push(photo.user.username);
+		};
+		$scope.users.sort();
+	}	
 }
